5dcf89a31f6832ff993586e771c661ea66ae9b77
Simon Escobar <snescobar25@gmail.com>
2018-04-26 23:25:17 -0400

    adding new changes

---
+++ js/index.js
@@ -0,0 +1,30 @@
+var home = $("#home");
+var prj = $("#prj");
+var about = $("#about");
+var hf = $(".header-fondo");
+var me = $(".img-yo");
+
+home.click(() => {
+	hf.css({
+		"background-image":" url('../images/fondo.jpeg')",
+	})
+	hf.addClass("animati")
+	me.delay(800).removeClass("disable")
+});
+
+
+prj.click(() => {
+	hf.css({
+		"background-image":" url('../images/projects.jpeg')",
+	})
+	hf.addClass("animati")
+	me.addClass("disable")
+});
+
+about.click(() => {
+	hf.css({
+		"background-image":" url('../images/about.jpeg')",
+	})
+	hf.addClass("animati")
+	me.addClass("disable")
+});
