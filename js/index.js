4819ecdd2d0cae7b7d4245e700254ecd556559b8
Simon Escobar <snescobar25@gmail.com>
2018-04-27 00:52:18 -0400

    acomodar list-prjs

--- js/index.js
+++ js/index.js
@@ -3,13 +3,16 @@ var prj = $("#prj");
 var about = $("#about");
 var hf = $(".header-fondo");
 var me = $(".img-yo");
+var list = $(".list-projects");
 
 home.click(() => {
 	hf.css({
 		"background-image":" url('../images/fondo.jpeg')",
 	})
 	hf.addClass("animati")
-	me.delay(800).removeClass("disable")
+	me.removeClass("disable")
+	me.addClass("animati")
+	list.addClass("disable")
 });
 
 
@@ -17,6 +20,7 @@ prj.click(() => {
 	hf.css({
 		"background-image":" url('../images/projects.jpeg')",
 	})
+	list.removeClass("disable")
 	hf.addClass("animati")
 	me.addClass("disable")
 });
@@ -25,6 +29,7 @@ about.click(() => {
 	hf.css({
 		"background-image":" url('../images/about.jpeg')",
 	})
+	list.addClass("disable")
 	hf.addClass("animati")
 	me.addClass("disable")
 });
