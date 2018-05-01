cc177fad2d3c3aa630c608d8cbd461ce812ad640
Simon Escobar <snescobar25@gmail.com>
2018-05-01 01:16:51 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,30 +1,7 @@
-var home = $("#home");
-var prj = $("#prj");
-var about = $("#about");
-var hf = $(".header-fondo");
-var me = $(".img-yo");
-
-home.click(() => {
-	hf.css({
-		"background-image":" url('/new-portfolio1/images/fondo.jpeg')",
-	})
-	hf.addClass("animati")
-	me.delay(800).removeClass("disable")
-});
-
-
-prj.click(() => {
-	hf.css({
-		"background-image":" url('/new-portfolio1/images/projects.jpeg')",
-	})
-	hf.addClass("animati")
-	me.addClass("disable")
-});
-
-about.click(() => {
-	hf.css({
-		"background-image":" url('/new-portfolio1/images/about.jpeg')",
-	})
-	hf.addClass("animati")
-	me.addClass("disable")
-});
+var home=$("#home");var prj=$("#prj");var about=$("#about");var hf=$(".header-fondo");var me=$(".img-yo");home.click(()=>{hf.css({"background-image":" url('/new-portfolio1/images/fondo.jpeg')",})
+hf.addClass("animati")
+me.delay(800).removeClass("disable")});prj.click(()=>{hf.css({"background-image":" url('/new-portfolio1/images/projects.jpeg')",})
+hf.addClass("animati")
+me.addClass("disable")});about.click(()=>{hf.css({"background-image":" url('/new-portfolio1/images/about.jpeg')",})
+hf.addClass("animati")
+me.addClass("disable")})
