9d8b89415e80d545b6be90e76fce010cb689ddc3
Simón Escobar <snsimonescobar@gmail.com>
2022-12-22 23:25:04 -0500

    Update html.js

--- src/html.js
+++ src/html.js
@@ -19,7 +19,21 @@ export default function HTML({
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
         {headComponents}
+
+        <!-- Hotjar Tracking Code for https://simonescob.github.io/ -->
+        <script>
+            (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:3299090,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        </script>
+
       </head>
       <body {...bodyAttributes}>
         {preBodyComponents}
